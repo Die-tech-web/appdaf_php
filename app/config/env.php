7b31b0f5dbd27e86218eef6b56620bf9877232c2
Die-tech-web <dieniang32@gmail.com>
2025-07-23 21:10:12 +0000

    Unignore .env and add it to repository b

--- app/config/env.php
+++ app/config/env.php
@@ -1,20 +1,18 @@
 <?php
-
-use Dotenv\Dotenv;
-
+use Dotenv\Dotenv; // ✅ Correct
 
 
-if (file_exists(__DIR__ . '/../../.env')) {
-    // ✅ Environnement local : charger le fichier .env
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
-    $dotenv->load();
-}
 
-// 🔁 Récupération des variables d'environnement (local ou Render)
-define('DB_HOST', getenv('DB_HOST') ?: $_ENV['DB_HOST'] ?? '');
-define('DB_PORT', getenv('DB_PORT') ?: $_ENV['DB_PORT'] ?? '');
-define('DB_DRIVE', getenv('DB_DRIVE') ?: $_ENV['DB_DRIVE'] ?? '');
-define('DB_USER', getenv('DB_USER') ?: $_ENV['DB_USER'] ?? '');
-define('DB_PASSWORD', getenv('DB_PASSWORD') ?: $_ENV['DB_PASSWORD'] ?? '');
-define('DB_NAME', getenv('DB_NAME') ?: $_ENV['DB_NAME'] ?? '');
-define('METHODE_INSTANCE_NAME', getenv('METHODE_INSTANCE_NAME') ?: $_ENV['METHODE_INSTANCE_NAME'] ?? '');
+$dotenv = Dotenv::createImmutable('../');
+
+$dotenv->load();
+
+define('DB_HOST', $_ENV['DB_HOST']);
+define('DB_PORT', $_ENV['DB_PORT']);
+define('DB_DRIVE', $_ENV['DB_DRIVE']);
+define('DB_USER', $_ENV['DB_USER']);
+define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
+define('DB_NAME', $_ENV['DB_NAME']);
+define('METHODE_INSTANCE_NAME', $_ENV['METHODE_INSTANCE_NAME']);
+
+
