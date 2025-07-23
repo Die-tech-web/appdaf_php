a3d0a607b05aeacbb42549cbb14fbd89ecafaec2
Die-tech-web <dieniang32@gmail.com>
2025-07-23 20:57:39 +0000

    dockerfile f

--- Dockerfile
+++ Dockerfile
@@ -31,10 +31,10 @@ RUN composer install --no-dev --optimize-autoloader || true
 RUN rm /etc/nginx/sites-enabled/default
 
 # Copier ta configuration nginx et supervisord
-COPY ./nginx/default.conf /etc/nginx/conf.d/default.conf
+COPY ./default.conf /etc/nginx/conf.d/default.conf
 COPY supervisord.conf /etc/supervisord.conf
 
 # Exposer le port
-EXPOSE 81
+EXPOSE 80
 
 CMD ["/usr/bin/supervisord", "-c", "/etc/supervisord.conf"]
--- app/config/env.php
+++ app/config/env.php
@@ -2,16 +2,19 @@
 
 use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable('../');
-
-$dotenv->load();
-
-define('DB_HOST', $_ENV['DB_HOST']);
-define('DB_PORT', $_ENV['DB_PORT']);
-define('DB_DRIVE', $_ENV['DB_DRIVE']);
-define('DB_USER', $_ENV['DB_USER']);
-define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
-define('DB_NAME', $_ENV['DB_NAME']);
-define('METHODE_INSTANCE_NAME', $_ENV['METHODE_INSTANCE_NAME']);
 
 
+if (file_exists(__DIR__ . '/../../.env')) {
+    // ✅ Environnement local : charger le fichier .env
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+    $dotenv->load();
+}
+
+// 🔁 Récupération des variables d'environnement (local ou Render)
+define('DB_HOST', getenv('DB_HOST') ?: $_ENV['DB_HOST'] ?? '');
+define('DB_PORT', getenv('DB_PORT') ?: $_ENV['DB_PORT'] ?? '');
+define('DB_DRIVE', getenv('DB_DRIVE') ?: $_ENV['DB_DRIVE'] ?? '');
+define('DB_USER', getenv('DB_USER') ?: $_ENV['DB_USER'] ?? '');
+define('DB_PASSWORD', getenv('DB_PASSWORD') ?: $_ENV['DB_PASSWORD'] ?? '');
+define('DB_NAME', getenv('DB_NAME') ?: $_ENV['DB_NAME'] ?? '');
+define('METHODE_INSTANCE_NAME', getenv('METHODE_INSTANCE_NAME') ?: $_ENV['METHODE_INSTANCE_NAME'] ?? '');
